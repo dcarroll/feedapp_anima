c4a1fbd897d0874c05e9d56920eda3add53ee583
Dave Carroll <djcarroll+git@gmail.com>
2015-02-10 17:17:18 -0800

    added contact list component as navigation option

---
+++ metadata/aura/ContactList/ContactListController.js
@@ -0,0 +1,9 @@
+({
+	doInit : function(component, event, helper) {
+		var action = component.get("c.getContacts");
+        action.setCallback(this, function(data) {
+            component.set("v.contacts", data.getReturnValue());
+        });
+        $A.enqueueAction(action);		
+	}
+})
